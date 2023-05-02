76d36939311d11d395095e7514e89bd068b1a241
jorgemedinabaez <131567985+jorgemedinabaez@users.noreply.github.com>
2023-05-02 01:17:40 -0400

    Ejercicios de comprobación y evaluación, sesión 4.

---
+++ drilling4.py
@@ -0,0 +1,20 @@
+persona1 = {
+    "nombre": "Jorge",
+    "apellido": "Medina",
+    "teléfono": 85443814
+}
+
+persona2 = {
+    "nombre": "Javier",
+    "apellido": "Araujo",
+    "teléfono": 98765432
+}
+
+persona3 = {
+    "nombre": "Paola",
+    "apellido": "Muñoz",
+    "teléfono": 12345678
+}
+
+listado = [persona1, persona2, persona3]
+print(listado)
---
+++ rebex4.py
@@ -0,0 +1,7 @@
+a = 8
+b = 10.5
+c = "ejercicio"
+d = {a, b, c}
+e = False
+f = [d, e]
+print(f)
